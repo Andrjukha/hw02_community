69b24da30fdc3086d41720b59167231d65d257c1
Andrey Pigolkin <a.pigolkin@gmail.com>
2022-08-31 09:49:55 +0300

    Спринт3 финал1

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -4,19 +4,20 @@ from .models import Post
 
 
 class GroupAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'title', 'slug', 'description')
-    empty_value_display='-пусто-'
+    list_display = ("pk", "title", "slug", "description")
+    empty_value_display = "-пусто-"
 
 
 class PostAdmin(admin.ModelAdmin):
     # Перечисляем поля, которые должны отображаться в админке
-    list_display = ('pk', 'text', 'pub_date', 'author', 'group') 
+    list_display = ("pk", "text", "pub_date", "author", "group")
     # Добавляем интерфейс для поиска по тексту постов
-    search_fields = ('text',) 
+    search_fields = ("text",)
     # Добавляем возможность фильтрации по дате
-    list_filter = ('pub_date',) 
-    list_editable = ('group',)
-    empty_value_display='-пусто-'
+    list_filter = ("pub_date",)
+    list_editable = ("group",)
+    empty_value_display = "-пусто-"
 
-admin.site.register(Post, PostAdmin) 
-admin.site.register(Group, GroupAdmin) 
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Group, GroupAdmin)
