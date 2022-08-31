0051fd7ec5a6615fea02891ede3884473c9f7a4f
Andrey Pigolkin <a.pigolkin@gmail.com>
2022-08-31 09:35:49 +0300

    Спринт3 финал1

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from .models import Group
 from .models import Post
+
+
 class GroupAdmin(admin.ModelAdmin):
-    list_display = ('pk','title', 'slug', 'description')
+    list_display = ('pk', 'title', 'slug', 'description')
     empty_value_display='-пусто-'
+
+
 class PostAdmin(admin.ModelAdmin):
     # Перечисляем поля, которые должны отображаться в админке
-    list_display = ('pk','text', 'pub_date', 'author','group') 
+    list_display = ('pk', 'text', 'pub_date', 'author', 'group') 
     # Добавляем интерфейс для поиска по тексту постов
     search_fields = ('text',) 
     # Добавляем возможность фильтрации по дате
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,34 +1,28 @@
-from typing import Any
+# from typing import Any
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-#from django.http import HttpResponse
-from .models import Post,Group
+# from django.http import HttpResponse
+from .models import Post, Group
 
 # Главная страница
-def index(request):
+def  index(request):
     # Одна строка вместо тысячи слов на SQL:
     # в переменную posts будет сохранена выборка из 10 объектов модели Post,
-    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    # отсортированных по полю pub_date по убыванию 
+    # (от больших значений к меньшим)
     posts = Post.objects.order_by('-pub_date')[:10]
     # В словаре context отправляем информацию в шаблон
     context = {
-        'posts': posts,
+        'posts': posts, 
     }
     return render(request, 'posts/index.html', context)  
 
-def group_posts(request,slug):
+def  group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context ={
-        'group': group,
+        'group' : group,
         'posts' : posts,
     }
-    
     return render(request, 'posts/group_list.html', context)  
-    
-
-
-
-
-
